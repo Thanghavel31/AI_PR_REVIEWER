a0f5c0b50fe11267e849bb055d305923907e9c08
Thanghavel S <thanghavel.saravanan@stellantis.com>
2025-06-16 15:51:19 +0530

    base

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        // This is the entry point of the application.
+        // You can add code here to initialize or run your application logic.
+        System.out.println("Hello, World!");
+    }
+}
